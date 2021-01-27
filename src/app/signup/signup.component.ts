4ec93104b48515d18d4de9f6e27de851a0a0b852
aharon <aharonhezi1@gmail.com>
2021-01-27 13:33:15 +0200

    spliting signup and login to 2 components. moving to reactive forms

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -34,7 +34,7 @@ export class SignupComponent implements OnInit {
       email:['',[Validators.required, Validators.email]],
       userName: ['', [Validators.required, Validators.minLength(2)]],
       password: ['', [Validators.required, Validators.minLength(2)]],
-      confirmPassword:[[Validators.required, Validators.minLength(2)]],
+      confirmPassword:['',[Validators.required, Validators.minLength(2)]],
     
     })
   }
